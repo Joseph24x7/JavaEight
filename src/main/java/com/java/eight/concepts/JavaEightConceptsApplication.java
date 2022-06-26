bff363b798d965401799a08e205db04c78149dce
Joseph24x7 <Joseph2471994@gmail.com>
2022-06-26 17:55:36 +0530

    build auto test

--- src/main/java/com/java/eight/concepts/JavaEightConceptsApplication.java
+++ src/main/java/com/java/eight/concepts/JavaEightConceptsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaEightConceptsApplication {
+public class JavaEightConceptsApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEightConceptsApplication.class, args);
